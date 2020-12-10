bf6bf8b7762813f8ba2e3e05f7cb730b2a5b6fd9
Laude Francois Espiritu <laude.espiritu@ytpi.com>
2020-12-10 17:02:35 +0800

    finished design and functionalities

---
+++ assets/dist/js/custom.js
@@ -0,0 +1,81 @@
+// Display Loading Overlay
+function loadingOverlay(display) {
+    document.getElementById("overlay_wrapper").style.display = display;
+}
+
+$(function() {
+    // Data Picker Initialization
+    $("#txtStartDate, #txtEndDate").datepicker();
+
+    // Close Form
+    $(".btn-form-close").on("click", function() {
+        $(".overlay, .project-form").css("display", "none");
+    });
+
+    // Open Form
+    $("#addproject").on("click", function() {
+        $(".overlay, .project-form").css("display", "block");
+    });
+
+    const donors = document.querySelector("#txtDonors");
+    const sectors = document.querySelector("#txtSectors");
+
+    // Donors input form tags input
+    const tagifyDonors = new Tagify(donors, {
+        whitelist: [
+            "Enim",
+            "Blandit",
+            "Volutpat",
+            "Maecenas",
+            "Consectetur",
+            "Dictum",
+            "Scelerisque"
+            ],
+        dropdown: {
+            position: "input",
+            enabled : 0 // always opens dropdown when input gets focus
+        }
+    });
+
+    // Sectors input form tags input
+    const tagifySectors = new Tagify(sectors, {
+        whitelist: [
+            "Enim",
+            "Blandit",
+            "Volutpat",
+            "Maecenas",
+            "Consectetur",
+            "Dictum",
+            "Scelerisque"
+            ],
+        dropdown: {
+            position: "input",
+            enabled : 0 // always opens dropdown when input gets focus
+        }
+    });
+
+    // Sample auto suggestion values
+    var statusTags = [
+        "Enim",
+        "Blandit",
+        "Volutpat",
+        "Maecenas",
+        "Consectetur",
+        "Dictum",
+        "Scelerisque"
+        ];
+
+    $("#txtStatus, #txtDivision, #txtDonors, #txtSectors").autocomplete({
+        source: statusTags,
+        minLength: 0
+    }).focus(function () {
+        $(this).autocomplete("search", "");
+    });
+
+    // Display loading overlay
+    $("#btnSave").on("click", function () {
+        loadingOverlay("block");
+        // After 3 seconds, page will refresh to remove overlay loading screen
+        setTimeout(function () { location.reload(true); }, 3000);
+    });
+});
